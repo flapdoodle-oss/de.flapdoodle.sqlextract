b35ffd8e7371b5d81d2ac239d2c3fcea67800be0
Michael Mosmann <michael@mosmann.de>
2021-05-18 09:31:35 +0200

    table filter list

--- src/main/kotlin/de/flapdoodle/sqlextract/config/TableFilterList.kt
+++ src/main/kotlin/de/flapdoodle/sqlextract/config/TableFilterList.kt
@@ -4,28 +4,37 @@ import de.flapdoodle.sqlextract.db.Name
 import de.flapdoodle.sqlextract.filetypes.Attributes
 
 data class TableFilterList(
-    val filter: List<TableFilter>
+    val filter: List<TableFilter>,
+    val excludedSchemas: Set<String>
 ) {
     private val filterBySchema = filter.associateBy { it.schema }
+
     init {
         val groupedBySchema = filter.groupBy { it.schema }
-        val sameSchema = groupedBySchema.filter { it.value.size!=1 }
-        require(sameSchema.isEmpty()) {"same schema used more than once: $sameSchema"}
+        val sameSchema = groupedBySchema.filter { it.value.size != 1 }
+        require(sameSchema.isEmpty()) { "same schema used more than once: $sameSchema" }
+        val schemaHasFilter = excludedSchemas.filter { filterBySchema.containsKey(it) }
+        require(schemaHasFilter.isEmpty()) { "excluded schema has filter definitions: $schemaHasFilter" }
     }
 
     fun matchingTableName(name: Name): Boolean {
-        val match = filterBySchema[name.schema]
-        
-        return match?.matchingTableName(name) ?: true
+        return if (excludedSchemas.contains(name.schema)) false
+        else
+            filterBySchema[name.schema]?.matchingTableName(name) ?: true
     }
 
     companion object {
         fun parse(source: Attributes.Node?): TableFilterList {
+            val excludeSchemas = source?.findValues("excludeSchema", String::class)?.toSet()
+
             val tableFilter = source?.nodeKeys()?.map {
                 TableFilter.parse(it, source.get(it, Attributes.Node::class))
             }
 
-            return TableFilterList(tableFilter ?: emptyList())
+            return TableFilterList(
+                filter = tableFilter ?: emptyList(),
+                excludedSchemas = excludeSchemas ?: emptySet()
+            )
         }
     }
 }
