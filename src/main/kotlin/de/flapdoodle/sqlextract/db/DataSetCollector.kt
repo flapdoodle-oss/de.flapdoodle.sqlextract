a0b2c8a9ee609a0432b321029ab697593388132b
Michael Mosmann <michael@mosmann.de>
2021-05-20 08:09:14 +0200

    collector first try

---
+++ src/main/kotlin/de/flapdoodle/sqlextract/db/DataSetCollector.kt
@@ -0,0 +1,164 @@
+package de.flapdoodle.sqlextract.db
+
+import de.flapdoodle.sqlextract.graph.TableGraph
+import de.flapdoodle.sqlextract.jdbc.query
+import java.sql.Connection
+
+class DataSetCollector(
+    val connection: Connection,
+    val tables: Tables
+) {
+    private val tableGraph = TableGraph.of(tables.all())
+
+    fun collect(tableName: Name, query: String) {
+        val filteredGraph = tableGraph.filter(tableName)
+        val table = tables.get(tableName)
+        val rows = Rows()
+
+        collect(table, filteredGraph, rows, query)
+    }
+
+    private fun collect(table: Table, filteredGraph: TableGraph, rows: Rows, query: String, parameters: Map<Int, Any> = emptyMap()) {
+        println("---------------------------------------------")
+        println("collect $query with $parameters")
+
+        val newRows = connection.query {
+            val statement = prepareStatement(query)
+            parameters.forEach { index, value ->
+                statement.setObject(index, value)
+            }
+            statement.executeQuery()
+        }
+            .map {
+                TableRow.of(table, table.columns.map { column ->
+                    column.name to this.column(column.name, Object::class)
+                }.toMap())
+            }
+
+        val missingRows = rows.missingRows(newRows)
+        rows.add(newRows)
+
+        if (missingRows.isNotEmpty()) {
+            val tablesPointingFrom = filteredGraph.referencesTo(table.name)
+            tablesPointingFrom.forEach { from ->
+                val fromTable = tables.get(from)
+                collect(fromTable, filteredGraph, rows, missingRows) { row -> constraintsOf(fromTable, row) }
+            }
+            val tablesPointingTo = filteredGraph.referencesFrom(table.name)
+            tablesPointingTo.forEach {  to ->
+                val toTable = tables.get(to)
+                collect(toTable, filteredGraph, rows, missingRows) { row -> constraintsOf(row, toTable) }
+            }
+        }
+    }
+
+    private fun collect(
+        table: Table,
+        filteredGraph: TableGraph,
+        rows: Rows,
+        newRows: List<TableRow>,
+        constraintsFactory: (TableRow) -> List<Pair<String, Any?>>
+    ) {
+        newRows.forEach { row ->
+            val constraints = constraintsFactory(row)
+            val constraintsSql = constraints.map { it.first }.joinToString(separator = " and ")
+
+            val query = "select * from ${table.name.asSQL()} where $constraintsSql"
+            val parameters = constraints
+                .filter { it.second!=null }
+                .mapIndexed{ index, pair -> index+1 to pair.second !! }
+                .toMap()
+
+            collect(table, filteredGraph,rows, query, parameters)
+        }
+    }
+
+    private fun constraintsOf(from: Table, to: TableRow): List<Pair<String, Any?>> {
+        val fk = from.foreignKeys.filter { it.destinationTable == to.table.name.name }
+        require(fk.isNotEmpty()) {"expected foreign keys to ${to.table} from ${from.name}"}
+        return fk.map {
+            val value = to.values[it.destinationColumn]
+            if (value!=null) {
+                "${it.sourceColumn} = ?" to value
+            } else {
+                "${it.sourceColumn} is null" to null
+            }
+        }
+    }
+
+    private fun constraintsOf(from: TableRow, to: Table): List<Pair<String, Any?>> {
+        val fk = from.table.foreignKeys.filter { it.destinationTable == to.name.name }
+        require(fk.isNotEmpty()) {"expected foreign keys to ${from.table} from ${to.name}"}
+        return fk.map {
+            val value = from.values[it.sourceColumn]
+            if (value!=null) {
+                "${it.destinationColumn} = ?" to value
+            } else {
+                "${it.destinationColumn} is null" to null
+            }
+        }
+    }
+    class Rows {
+        private var tableRowsMap = emptyMap<Name, TableRows>()
+
+        fun add(rows: List<TableRow>) {
+            rows.forEach { row ->
+                entryFor(row.table.name)
+                    .add(row)
+            }
+        }
+
+        private fun entryFor(table: Name): TableRows {
+            val ret = tableRowsMap[table]
+            if (ret == null) {
+                val rows = TableRows(table)
+                tableRowsMap = tableRowsMap + (table to rows)
+                return rows
+            } else {
+                return ret
+            }
+        }
+
+        fun missingRows(rows: List<TableRow>): List<TableRow> {
+            return rows.filter { row ->
+                entryFor(row.table.name).contains(row)
+            }
+        }
+    }
+
+    class TableRows(val table: Name) {
+        private var rowMap = emptyMap<RowKey, TableRow>()
+
+        fun add(row: TableRow) {
+            require(row.table.name == table) { "wrong table: $row != $table" }
+            if (!rowMap.containsKey(row.key)) {
+                rowMap = rowMap + (row.key to row)
+            }
+        }
+
+        fun contains(row: TableRow): Boolean {
+            return rowMap.containsKey(row.key)
+        }
+    }
+
+    data class TableRow(val table: Table, val key: RowKey, val values: Map<String, Any?>) {
+
+        companion object {
+            fun of(table: Table, values: Map<String, Any?>): TableRow {
+                return TableRow(table, rowKey(table, values), values)
+            }
+
+            private fun rowKey(table: Table, values: Map<String, Any?>): RowKey {
+                return RowKey(table.primaryKeys.map {
+                    it.columnName to knownType(values[it.columnName])
+                }.toMap())
+            }
+
+            private fun <T> knownType(value: T?): T? {
+                return value
+            }
+        }
+    }
+
+    data class RowKey(val key: Map<String, Any?>)
+}
--- src/main/kotlin/de/flapdoodle/sqlextract/db/Extractor.kt
+++ src/main/kotlin/de/flapdoodle/sqlextract/db/Extractor.kt
@@ -5,7 +5,6 @@ import de.flapdoodle.sqlextract.config.ForeignKeys
 import de.flapdoodle.sqlextract.graph.TableGraph
 import de.flapdoodle.sqlextract.jdbc.Connections
 import de.flapdoodle.sqlextract.jdbc.query
-import de.flapdoodle.sqlextract.jdbc.table
 import de.flapdoodle.sqlextract.jdbc.tables
 
 
@@ -17,77 +16,88 @@ class Extractor {
         val connection =
             Connections.connection(config.jdbcUrl, config.className, config.user, config.password, config.driver)
 
-        val tableResolver = CachingTableResolverWrapper(JdbcTableResolver(
-            connection = connection,
-            postProcess = addForeignKeys(config.foreignKeys)
-        ))
+        connection.use { con ->
+            val tableResolver = CachingTableResolverWrapper(
+                JdbcTableResolver(
+                    connection = connection,
+                    postProcess = addForeignKeys(config.foreignKeys)
+                )
+            )
 
-        println("All Tables")
-        println("-------------------------")
-        val tableNames = connection.metaData.tables().map {  table ->
+            println("All Tables")
+            println("-------------------------")
+            val tableNames = connection.metaData.tables().map { table ->
 //            println("--> ${table.name}")
-                    table.name
+                table.name
             }
-        println("-------------------------")
-        println()
+            println("-------------------------")
+            println()
 
-        val includedTables = tableNames.filter(config.tableFilter::matchingTableName)
+            val includedTables = tableNames.filter(config.tableFilter::matchingTableName)
 
-        println("Tables")
-        println("-------------------------")
-        includedTables.groupBy { it.schema }.forEach { schema, list ->
-            println("Schema: $schema")
-            list.forEach {
-                println("-> ${it.name}")
+            println("Tables")
+            println("-------------------------")
+            includedTables.groupBy { it.schema }.forEach { schema, list ->
+                println("Schema: $schema")
+                list.forEach {
+                    println("-> ${it.name}")
+                }
             }
-        }
 //        includedTables.forEach {
 //            println("-> $it")
 //        }
-        println("-------------------------")
+            println("-------------------------")
 
-        val tables = Tables.empty().add(includedTables, tableResolver)
-        val tableGraph = TableGraph.of(tables.all())
+            val tables = Tables.empty().add(includedTables, tableResolver)
+            val tableGraph = TableGraph.of(tables.all())
 
-        connection.use { con ->
+            val dataSetCollector = DataSetCollector(
+                connection = connection,
+                tables = tables
+            )
+
             config.dataSets.forEach { dataSet ->
-                println("-> ${dataSet.name}")
-
-//                val dataSetTables = Tables.empty()
-//                    .add(dataSet.include + dataSet.table, tableResolver)
-
-                val table = tables.get(dataSet.table)
-
                 val sqlQuery = "select * from ${dataSet.table.asSQL()} where ${dataSet.where}"
-
-                println("query: $sqlQuery")
-                con.query { prepareStatement(sqlQuery).executeQuery() }
-                    .map {
-                        table.columns.forEach { column ->
-                            val value = column(column.name, Object::class)
-                            println("${column.name}=$value")
-                        }
-                    }
-//                val statement = connection.prepareStatement(sqlQuery)
-//                val resultSet = statement.executeQuery()
-//                while (resultSet.next()) {
-//                    println("-> "+resultSet)
-//                }
+                dataSetCollector.collect(dataSet.table, sqlQuery)
             }
+
+//            config.dataSets.forEach { dataSet ->
+//                println("-> ${dataSet.name}")
+//
+////                val dataSetTables = Tables.empty()
+////                    .add(dataSet.include + dataSet.table, tableResolver)
+//
+//                val table = tables.get(dataSet.table)
+//
+//                val sqlQuery = "select * from ${dataSet.table.asSQL()} where ${dataSet.where}"
+//
+//                println("query: $sqlQuery")
+//                con.query { prepareStatement(sqlQuery).executeQuery() }
+//                    .map {
+//                        table.columns.forEach { column ->
+//                            val value = column(column.name, Object::class)
+//                            println("${column.name}=$value")
+//                        }
+//                    }
+////                val statement = connection.prepareStatement(sqlQuery)
+////                val resultSet = statement.executeQuery()
+////                while (resultSet.next()) {
+////                    println("-> "+resultSet)
+////                }
+//            }
 //            val rs: ResultSet = it.metaData.getTables(null, null, "%", null)
 //            while (rs.next()) {
 //                println("-> "+rs.getString(3))
 //            }
+            println("----------------")
+            println(tableGraph.asDot())
+            println("----------------")
         }
-
-        println("----------------")
-        println(tableGraph.asDot())
-        println("----------------")
     }
 
     private fun addForeignKeys(foreignKeys: List<ForeignKeys>): (Table) -> Table {
         return {
-            val keys = foreignKeys.filter { fk -> fk.schema==it.name.schema }
+            val keys = foreignKeys.filter { fk -> fk.schema == it.name.schema }
                 .flatMap { fk -> fk.foreignKeys(it.name.name) }
 
             it.withForeignKeys(keys)
